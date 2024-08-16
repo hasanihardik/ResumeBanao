0a36c45707a8e62560fcc857be99b54b40aed9c9
hasanihardik <hasanihardik@gmail.com>
2024-08-16 17:18:56 +0530

    app and index js setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import FormContainer from "./Components/FormContainer";
+import {useEffect} from "react"
 function App() {
+  useEffect(()=>{
+    document.title="ResumeBanao"
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FormContainer />
     </div>
   );
 }
